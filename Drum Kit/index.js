ce023a6aa714e507682b2171c21839e9ca053d43
Gregory Szaro <gregoryszaro@gmail.com>
2023-03-07 12:20:49 -0500

    added event listeners to buttons

---
+++ Drum Kit/index.js
@@ -0,0 +1,13 @@
+let numOfDrumButtons = document.querySelectorAll('.drum').length;
+
+for (let i = 0; i < numOfDrumButtons; i++){
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
+        alert('I got clicked!');
+    
+    });
+    
+}
+
+
+
