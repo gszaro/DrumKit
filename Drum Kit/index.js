e7046f8abc8f78eee9ccdc58dfe0b6c6edf7d340
Gregory Szaro <gregoryszaro@gmail.com>
2023-03-07 16:43:28 -0500

    adds sounds to buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,20 +1,42 @@
 let numOfDrumButtons = document.querySelectorAll('.drum').length;
 let audio = new Audio('sounds.tom-1.mp3');
 
-for (let i = 0; i < numOfDrumButtons; i++){
+for (let i = 0; i < numOfDrumButtons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    let buttonInnerHTML = this.innerHTML;
 
-    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
-        
-        this.style.color = 'white';
-    
-    });
-    
+    switch (buttonInnerHTML) {
+      case 'w':
+        let kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+      case 'a':
+        let snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+      case 's':
+        let crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+      case 'd':
+        let tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+      case 'j':
+        let tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+      case 'k':
+        let tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+      case 'l':
+        let tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+      default:
+        console.log(buttonInnerHTML);
+        break;
+    }
+  });
 }
-
-
-
-
-
-
-// let audio = new Audio('sounds/tom-1.mp3');
-//         audio.play();
